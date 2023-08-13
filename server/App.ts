b5cd08cfc247ba9e0496bd36cb31e559df874bb0
Mateusz Krzysiek <73441601+christaf@users.noreply.github.com>
2023-08-13 17:11:24 +0200

    added config file, app is listening to specified port, added dependencies

--- server/App.ts
+++ server/App.ts
@@ -1,5 +1,7 @@
-import {app, ServerStart} from './src/Server/index';
+import {app, ServerStart} from './src/Server';
 import Connection from './src/Database/index';
 
 const Connect = new Connection()
-ServerStart(Connect);
+ServerStart(Connect).then(
+    () => console.log("Server started")
+);
--- server/src/Server/index.ts
+++ server/src/Server/index.ts
@@ -1,11 +1,18 @@
 import express from 'express'
 import Connection from "../Database";
+import config from "config"
 
 const app = express();
+const port = config.get("APP.PORT");
 
 async function ServerStart(Connection: Connection | null) {
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
+
+    app.listen(port, () => {
+        return console.log(`Serwer uruchomiony na porcie ${port}`);
+    });
+
 }
 
 export {app, ServerStart};
