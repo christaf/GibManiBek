8a6cd27d706c35454521be38f8bcb08c2275a081
Mateusz Krzysiek <73441601+christaf@users.noreply.github.com>
2023-09-05 00:36:45 +0200

    adjustment

--- server/src/Routes/contacts.ts
+++ server/src/Routes/contacts.ts
@@ -8,7 +8,8 @@ export async function getContacts(id: string) {
     });
     const userList = [];
     for (const id of friendsIdList) {
-        const user = await connection.findContactUserById(id);
+        //TODO HARD CODED ID = 1
+        const user = await connection.findContactUserById("1"); //podmienic na id
         userList.push(user);
     }
     return userList;
